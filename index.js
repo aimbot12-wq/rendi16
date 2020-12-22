b779987be1f4ace539eff9f3740a80c138eb28a5
aimbot12-wq <76447916+aimbot12-wq@users.noreply.github.com>
2020-12-22 23:36:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'Nama bot'; 
-const instagram = 'Instagram kamu'; 
+const BotName = 'Rendikur'; 
+const instagram = 'Rendi_kurniawan02'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Nama kamu\n' // Nama kamu
-            + 'ORG:Nama bot;\n' // Nama bot
+            + 'FN:rendi kurniawa\n' // Nama kamu
+            + 'ORG:rendi kur;\n' // Nama bot
             + 'TEL;type=CELL;type=VOICE;waid=6280000000000:+62 000-0000-0000\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
